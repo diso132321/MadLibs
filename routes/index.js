5758d5e1f27d893ad3644eda96a2ab3b6979e5ac
Diego Gonzalez Chavez 5613 <u4h8@fresnou.org>
2023-09-12 09:25:07 -0700

    Finishing MadLibs

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET home page. */
+router.post('/', function(req, res) {
+  let body = req.body;
+  let newStory = getStory(body);
+  res.render('story', {
+    newStory: newStory
+  });
+})
+
+
+
+
+
+module.exports = router;
+
+function getStory(formData) {
+  if (formData.storyChoice === "1") {
+    return generateStory1(formData);
+  } else if (formData.storyChoice === "2") {
+    return generateStory2(formData);
+  } else if (formData.storyChoice === "3") {
+    return generateStory3(formData);
+  } else {
+    return "invalid";
+  }
+}
